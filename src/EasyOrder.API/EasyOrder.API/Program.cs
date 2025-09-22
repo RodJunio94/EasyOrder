a25eb40fe138fb48e6bd730757a90c18139f280a
Junio Rodrigues <junio@buildbox.com.br>
2025-09-21 22:44:20 -0300

    Enable CORS and disable HTTPS redirection
    Added a default CORS policy to allow any origin, header, and method.
    Disabled `app.UseHttpsRedirection()` by commenting it out.
    Integrated `app.UseCors()` middleware to apply the CORS policy.

--- src/EasyOrder.API/EasyOrder.API/Program.cs
+++ src/EasyOrder.API/EasyOrder.API/Program.cs
@@ -12,6 +12,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => 
+options.AddDefaultPolicy(policy =>
+{
+    policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+}));
 
 // Register repositories for dependency injection
 builder.Services.AddSingleton<IOrderRepository, InMemoryOrderRepository>();
@@ -35,7 +40,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors();
 app.UseAuthorization();
 
 app.MapControllers();
